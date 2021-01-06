c030676893d264fc0cfbaa8307cc1a0dd54fd9b2
mallvielfrass <masterharoldsaxon4@gmail.com>
2021-01-06 19:52:59 +0300

    add error handle

--- web/main.go
+++ web/main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	DAL "github.com/slovaq/web2tg/web/DAL"
-	"html/template"
-	"net/http"
 )
 
 var DB = DAL.DB
+var debug bool
 
+func init() {
+	debug = os.Getenv("DEBUG") != ""
+}
+func chk(err error) {
+	if err != nil {
+		pc, fn, line, _ := runtime.Caller(1)
+		if debug {
+			log.Printf("[ERROR] [%s:%s:%d] %s ", runtime.FuncForPC(pc).Name(), fn, line, err)
+		} else {
+			log.Printf("[ERROR] [%s:%d] %s ", fn, line, err)
+		}
+	}
+}
 func index(writer http.ResponseWriter, _ *http.Request) {
 	var records []DAL.Record
 	var cities []DAL.City
@@ -41,23 +59,30 @@ func index(writer http.ResponseWriter, _ *http.Request) {
 		}
 	}
 }
+
 func main() {
-	errs := make([]error, 3, 3)
-	errs[0] = DB.AutoMigrate(&DAL.Record{})
-	errs[1] = DB.AutoMigrate(&DAL.User{})
-	errs[2] = DB.AutoMigrate(&DAL.City{})
-	for _, err := range errs {
-		if err != nil {
-			fmt.Printf("error in main() with text: %s \n", err.Error())
-		}
-	}
+	//	errs := make([]error, 3, 3)
+	//errs[0] = DB.AutoMigrate(&DAL.Record{})
+	//errs[1] = DB.AutoMigrate(&DAL.User{})
+	//errs[2] = DB.AutoMigrate(&DAL.City{})
+	err := DB.AutoMigrate(&DAL.Record{})
+	chk(err)
+	err = DB.AutoMigrate(&DAL.User{})
+	chk(err)
+	err = DB.AutoMigrate(&DAL.City{})
+	chk(err)
+	//	for _, err := range errs {
+	//		if err != nil {
+	//			fmt.Printf("error in main() with text: %s \n", err.Error())
+	//		}
+	//	}
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Handle("/static", http.FileServer(http.Dir("static")))
 	r.Route("/", func(r chi.Router) {
 		r.Get("/", index)
 	})
-	err := http.ListenAndServe(":1111", r)
+	err = http.ListenAndServe(":1111", r)
 	if err != nil {
 		fmt.Printf("Cant start server with error %s \n Exiting..", err)
 	}
