7d95088ab0955257ed48fb829be0f35215ef99a8
mallvielfrass <masterharoldsaxon4@gmail.com>
2021-01-07 16:15:53 +0300

    work with vapi lend

--- web/vapi/vapi.go
+++ web/vapi/vapi.go
@@ -1,15 +1,20 @@
 package vapi
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
 )
 
 func Router(r chi.Router) {
-	r.Get("/", vIndex)
+	log.Println(">vapi router")
+	r.Get("/index", vIndex)
 
 }
 func vIndex(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "template/home/index.html")
+	log.Println(">vapi index")
+	http.ServeFile(w, r, "vapi/template/home/index.html")
+	fmt.Fprintf(w, "v index")
 }
