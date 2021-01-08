b26642cb815d67b8ba0bdba3cba48141b5037333
rasongame <dubstepgunser@mail.ru>
2021-01-08 16:43:32 +0500

    города готовы?

--- web/API/City.go
+++ web/API/City.go
@@ -1,6 +1,7 @@
 package API
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/slovaq/web2tg/web/DAL"
 	"net/http"
@@ -8,20 +9,50 @@ import (
 )
 
 func CityCreate(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	error_sended := false
+	obj := JsonObject{
+		Success: false,
+		Result:  nil,
+		Error:   nil,
+	}
 	chat_id, err := strconv.Atoi(request.FormValue("name"))
 	if err != nil {
-		writer.Write([]byte("chat_id is not valid"))
-		return
+		chk(&obj, writer, err)
+		error_sended = true
 	}
 	city := DAL.CreateOrGetCity(request.FormValue("name"), chat_id)
-	writer.Write([]byte(fmt.Sprint(city)))
+	obj.Success = true
+	obj.Result = city
+	returnData, err := json.Marshal(obj)
+	if !error_sended {
+		writer.Write(returnData)
+
+	}
 }
 func CityGet(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	error_sended := false
+	result := JsonObject{
+		Success: false,
+		Result:  nil,
+		Error:   nil,
+	}
 	city := DAL.GetCity(request.FormValue("city_name"))
 	if city == nil {
-		writer.Write([]byte("city not found"))
+		chk(&result, writer, fmt.Errorf("city not found"))
+		error_sended = true
+	}
+	result.Result = city
+	returnData, err := json.Marshal(result)
+	if err != nil {
+		chk(&result, writer, err)
+		fmt.Println("error when marshaling")
+		fmt.Println(err.Error())
 		return
 	}
-	writer.Write([]byte(fmt.Sprint(city)))
+	if !error_sended {
+		writer.Write(returnData)
+	}
 
 }
--- web/API/User.go
+++ web/API/User.go
@@ -1,32 +1,56 @@
 package API
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/slovaq/web2tg/web/DAL"
 	"net/http"
 )
 
 func UserCreate(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+
+	obj := JsonObject{
+		Success: false,
+		Result:  nil,
+		Error:   nil,
+	}
 	user, err := DAL.CreateUser(
 		request.FormValue("login"),
 		request.FormValue("name"),
 		request.FormValue("password"),
 	)
 	if err != nil {
-		writer.Write([]byte(err.Error()))
-		return
+		obj.Error = err.Error()
+		obj.Success = false
 	}
-	writer.Write([]byte(fmt.Sprint(user)))
+	obj.Result = user
+	return_data, err := json.Marshal(obj)
+	if err != nil {
+		writer.Write([]byte("Server error"))
+	}
+	writer.Write(return_data)
 
 }
 func UserGet(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+
+	obj := JsonObject{
+		Success: false,
+		Result:  nil,
+		Error:   nil,
+	}
 	user, err := DAL.GetUser(
 		request.FormValue("login"),
 		request.FormValue("password"),
 	)
 	if err != nil {
-		writer.Write([]byte(err.Error()))
-		return
+		obj.Error = err.Error()
+		obj.Success = false
 	}
-	writer.Write([]byte(fmt.Sprint(user)))
+	obj.Result = user
+	return_data, err := json.Marshal(obj)
+	if err != nil {
+		writer.Write([]byte("Server error"))
+	}
+	writer.Write(return_data)
 }
