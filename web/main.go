51a97a6a51ad84e915e9c8aff1bce83859c55aef
mallvielfrass <masterharoldsaxon4@gmail.com>
2021-01-11 22:55:10 +0300

    auth

--- web/main.go
+++ web/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"runtime"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -101,17 +102,65 @@ func reg(w http.ResponseWriter, r *http.Request) {
 	}
 	tmpl.Execute(w, nil)
 }
+
+var epoch = time.Unix(0, 0).Format(time.RFC1123)
+var noCacheHeaders = map[string]string{
+	"Expires":         epoch,
+	"Cache-Control":   "no-cache, private, max-age=0",
+	"Pragma":          "no-cache",
+	"X-Accel-Expires": "0",
+}
+
 func authMiddleware(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for k, v := range noCacheHeaders {
+			w.Header().Set(k, v)
+		}
 
-		c, err := r.Cookie("name")
+		fmt.Println("start")
+		login, err := r.Cookie("login")
+		if err != nil {
+			fmt.Printf("[login] error %s\n", err.Error())
+
+			//fmt.Fprintf(w, "[login] error %s", err)
+			http.Redirect(w, r, "/reg", 301)
+			return
+
+		} else {
+			fmt.Println(login.Value)
+		}
+		password, err := r.Cookie("password")
 		if err != nil {
 			fmt.Println(err.Error())
+			if err != nil {
+				_, err := w.Write([]byte(err.Error()))
+				if err != nil {
+					fmt.Printf("error in index() with text: %s \n", err.Error())
+				}
+				http.Redirect(w, r, "/reg", 301)
+				return
+			}
 		} else {
-			fmt.Println(c.Value)
+			fmt.Println(password.Value)
 		}
-		if c == nil {
+		fmt.Printf("(authMiddleware)> user:%s|password: %s\n", login, password)
+		if login.Value == "" || password.Value == "" {
+			tmpl, err := template.ParseFiles("templates/error.html", "templates/base.html")
+			if err != nil {
+				_, err := w.Write([]byte(err.Error()))
+				if err != nil {
+					fmt.Printf("error in index() with text: %s \n", err.Error())
+				}
+				fmt.Fprintf(w, "error %s", err)
+				return
+			}
+			tmpl.Execute(w, nil)
+			return
+		}
+		_, useErr := DAL.GetUser(login.Value, password.Value)
+
+		if useErr != nil {
 			tmpl, err := template.ParseFiles("templates/error.html", "templates/base.html")
 			if err != nil {
 				_, err := w.Write([]byte(err.Error()))
