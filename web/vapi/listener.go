b6bf8d0f4b2fbbc3e6255896dd887729702187b1
rasongame <dubstepgunser@mail.ru>
2021-01-20 19:06:11 +0500

    сепаратизм

--- web/vapi/listener.go
+++ web/vapi/listener.go
@@ -4,23 +4,21 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/fatih/color"
 	"github.com/slovaq/web2tg/web/DAL"
 )
 
-var apiString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%d&text=%s"
+var (
+	apiString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%d&text=%s"
+	links     []Link
+)
 
 // @rsngmbot in telegram...
 func sendMessage(token string, url string, text string) {
-	yellow := color.New(color.FgYellow, color.Bold).SprintFunc()
-	red := color.New(color.FgRed, color.Bold).SprintFunc()
-	b := color.New(color.FgBlue, color.Bold).SprintFunc()
-	g := color.New(color.FgGreen, color.Bold).SprintFunc()
-	var links []Link
 	DAL.DB.Where("user_link = ?", url).Find(&links)
-	fmt.Printf("%s\n\ttoken: %s \n\tlink: %s \n\tmessage> %s\n", red("sendMessage>"), g(token), yellow(url), b(text))
+	fmt.Printf("%s\n\ttoken: %s \n\tlink: %s \n\tmessage> %s\n", redPrint("sendMessage>"), greenPrint(token), yellowPrint(url), bluePrint(text))
 	fmt.Println("len links:", len(links))
 	fmt.Println("links:", links)
+
 	iurl := fmt.Sprintf(apiString, token, links[0].ChatID, text)
 	fmt.Println("iurl>", iurl)
 	_, err := http.Get(iurl)
